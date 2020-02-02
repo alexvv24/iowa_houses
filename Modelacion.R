999134539fb9d1f983fdd72b13f2d57cf56805c7
Alex <alexvv24@hotmail.com>
2020-02-02 21:53:19 +0100

    Update Modelacion.R
    - Ruta cambiada
    - hasta variable LotConfig sin incluir

--- Modelacion.R
+++ Modelacion.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(corrplot)
 library(ggrepel)
 
-setwd("~/OneDrive/Estudio/Kaggle/IowaHouses/data")
+setwd("~/OneDrive/Estudio/Kaggle/iowa_houses/")
 train <- read.csv("train.csv", stringsAsFactors = F, na.strings = "NA")
 test <- read.csv("test.csv", stringsAsFactors = F, na.strings = "NA")
 
@@ -49,4 +49,39 @@ sort(colSums(sapply(df[na.cols], is.na)), decreasing = T)
 
 #### Na imputing #### 
 
-df[is.na(df$PoolQC),]
+library(plyr)
+library(dplyr)
+df$PoolQC[is.na(df$PoolQC)] <- "None"
+Qualities <- c('None' = 0, 'Po' = 1, 'Fa' = 2, 'TA' = 3, 'Gd' = 4, 'Ex' = 5)
+df$PoolQC <- as.integer(revalue(df$PoolQC, Qualities))
+df[df$PoolArea > 0 & df$PoolQC == 0, c('PoolArea', 'PoolQC', 'OverallQual')]
+df$PoolQC[2421] <- 2
+df$PoolQC[2504] <- 2
+df$PoolQC[2600] <- 3
+
+df$MiscFeature[is.na(df$MiscFeature)] <- "None"
+df$MiscFeature <- as.factor(df$ MiscFeature)
+
+df$Alley[is.na(df$Alley)] <- "None"
+df$Alley <- as.factor(df$Alley)
+
+df$Fence[is.na(df$Fence)] <- "None"
+df[!is.na(df$SalePrice), ] %>% 
+  group_by(Fence) %>% 
+  summarise(median = median(SalePrice), counts = n())
+df$Fence <- as.factor(df$Fence)
+
+df$FireplaceQu[is.na(df$FireplaceQu)] <- "None"
+df$FireplaceQu <- as.integer(revalue(df$FireplaceQu, Qualities))
+
+ggplot(df, aes(x = as.factor(Neighborhood), y = LotFrontage)) +
+  geom_bar(stat = "summary", fun.y = "median", fill = "blue") + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+for (i in 1:nrow(df)){
+  if (is.na(df$LotFrontage[i])) {
+    df$LotFrontage[i] <- as.integer(median(df$LotFrontage[df$Neighborhood == df[i, "Neighborhood"]], na.rm = T))
+  }
+}
+
+df$LotShape <- as.integer(revalue(df$LotShape, c("IR3" = 0, "IR2" = 1, "IR1" = 2, "Reg" = 3)))
+
